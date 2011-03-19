b326059e314704df99a56a1645923b631b335401
Lewis Schmidt <lewis.schmidt@gmail.com>
2011-03-19 02:08:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskman::Application.config.secret_token = '207129e396df3b274392b2eab5b6991ca4b463fe5fc6722bcb18d09716b1ec15f2ce7b6b1f87b38008c45fad2e21246fe0d12b7a89ae61b49cbaeab24f460afa'
